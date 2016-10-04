c0c3cc929693f503d7a13eb6af1c770e0e27d397
meripepanashvili <mpepa13@freeuni.edu.ge>
2016-10-04 18:11:40 +0400

    partners hover box shadow

--- Jumpstart Page/partners.js
+++ Jumpstart Page/partners.js
@@ -3,6 +3,6 @@ var partners_num = 11;
 function load_partners(){
 	var i = 1;
 	for(i; i<=partners_num; i++){
-		$('#partners').append('<img src="images/partners/' + i + '.svg"> </img>');
+		$('#partners').append('<a href=\'#\'> <img src="images/partners/' + i + '.svg"> </img> </a>');
 	}
 }
